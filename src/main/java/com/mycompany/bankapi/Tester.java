762b775552fdb29e6361b4384baab4867410f20e
teganj <teganjennings1@hotmail.com>
2021-12-15 18:03:52 +0000

    Adding in first resource

--- src/main/java/com/mycompany/bankapi/Tester.java
+++ src/main/java/com/mycompany/bankapi/Tester.java
@@ -12,18 +12,16 @@ package com.mycompany.bankapi;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response; 
+import javax.ws.rs.core.Response;
 
-        @Path("/hello") 
+@Path("/hello")
 
 public class Tester {
-    
-     @GET
- @Path("/{param}")
- public Response sayHelloWorld(@PathParam("param") String message) {
-String output = "Hello " + message + "!";
-return Response.status(200).entity(output).build();
- }
-} 
 
-   
+    @GET
+    @Path("/{param}")
+    public Response sayHelloWorld(@PathParam("param") String message) {
+        String output = "Hello " + message + "!";
+        return Response.status(200).entity(output).build();
+    }
+}
