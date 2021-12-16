70b500b67f41abf517e767ff84be5a052fe1912c
Eugene-20 <46289318+Eugene-20@users.noreply.github.com>
2021-12-16 11:06:21 +0000

    Update CustomerServices.java
    Adding code to services

--- src/main/java/com/mycompany/bankapi/services/CustomerServices.java
+++ src/main/java/com/mycompany/bankapi/services/CustomerServices.java
@@ -1,9 +1,45 @@
 package com.mycompany.bankapi.services;
 
-/**
- *
- * @author HP
+import com.mycompany.bankapi.database.CustomerDatabase;
+import com.mycompany.bankapi.models.Customer;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @author Eugene Omondi x18425924
  */
 public class CustomerServices {
+    CustomerDatabase db = new CustomerDatabase();
+    private List<Customer> list = db.getCustomerDB();
+
+    public List<Customer> getAllCustomers() {
+        return list;
+    }
+
+    public Customer getCustomer(int customer_id) {
+        return list.get(customer_id - 1);
+    }
+
+    public Customer createMessage(Customer c) {
+        c.setCustomerId(list.size() + 1);
+        list.add(c);
+        System.out.println("201 - resource created with path: /customers/" + String.valueOf(c.getCustomerId()));
+        System.out.println("Updated Message:" + c.printCustomer());
+        return c;
+    }
+
+    public List<Customer> getSearchMessages(String account, String customer) {
+        List<Customer> matcheslist = new ArrayList<>();
+
+        for (Customer q : getAllCustomers()) {
+            if ((account == null || q.getCustomer().equals(customer))
+                    && (customer == null || q.getCustomer().equals(customer))) {
+                matcheslist.add(q);
+            }
+        }
+        return matcheslist;
+    }
+
+
     
 }
