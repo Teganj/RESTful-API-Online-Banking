2d4b5fda61a59eb8aeb042cec1e42e36cee43725
Eugene-20 <46289318+Eugene-20@users.noreply.github.com>
2021-12-17 20:07:01 +0000

    Update CustomerDatabase.java

--- src/main/java/com/mycompany/bankapi/database/CustomerDatabase.java
+++ src/main/java/com/mycompany/bankapi/database/CustomerDatabase.java
@@ -18,12 +18,12 @@ public class CustomerDatabase {
     public CustomerDatabase() {
         if (init) {
 
-            Customer cus1 = new Customer(1, "John Smith", "address", "johnsmith@apiproject.ie", "PASS", "CPASS", accountDB);
-            Customer cus2 = new Customer(2, "James Wright", "address", "jameswright@apiproject.ie", "PASS", "CPASS", accountDB);
-            Customer cus3 = new Customer(3, "Sam Smith", "address", "samsmith@apiproject.ie", "PASS", "CPASS", accountDB);
-            Customer cus4 = new Customer(4, "Rob Tierny", "address", "robtierny@apiproject.ie", "PASS", "CPASS", accountDB);
-            Customer cus5 = new Customer(5, "Hannah Carpenter", "address", "hannahcarpenter@apiproject.ie", "PASS", "CPASS", accountDB);
-            Customer cus6 = new Customer(6, "Sally Jones", "address", "sallyjones@apiproject.ie", "PASS", "CPASS", accountDB);
+            Customer cus1 = new Customer(1, "John Smith", "33 St Clones Way,Co Limrick", "johnsmith@apiproject.ie", "PASS", "CPASS", accountDB);
+            Customer cus2 = new Customer(2, "James Wright", "5 Woodsgift Hall,Co Galway", "jameswright@apiproject.ie", "PASS", "CPASS", accountDB);
+            Customer cus3 = new Customer(3, "Sam Smith", "9 Kingsfort Lodge,Co Dublin ", "samsmith@apiproject.ie", "PASS", "CPASS", accountDB);
+            Customer cus4 = new Customer(4, "Rob Tierny", " 3 Bridge St, Co Mayo", "robtierny@apiproject.ie", "PASS", "CPASS", accountDB);
+            Customer cus5 = new Customer(5, "Hannah Carpenter", "77 Diamond way, Co Meath", "hannahcarpenter@apiproject.ie", "PASS", "CPASS", accountDB);
+            Customer cus6 = new Customer(6, "Sally Jones", "71 Heatherview, Co Meath", "sallyjones@apiproject.ie", "PASS", "CPASS", accountDB);
 
             customerDB.add(cus1);
             customerDB.add(cus2);
